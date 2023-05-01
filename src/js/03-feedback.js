9971e6df78dc0f955a2f94f743888f5a1f6248f9
LeraF825 <fertikovalera@gmail.com>
2023-05-01 21:19:44 +0300

    update task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -17,12 +17,30 @@ let formData = {
 function onFormData(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+
+  if (e.target.name === 'email' && message.value !== '') {
+    return;
+  }
+  if (e.target.name === 'message' && email.value !== '') {
+    return;
+  }
+
+  email.value = formData.email;
+  message.value = formData.message;
 }
 
 function onSubmitForm(e) {
   e.preventDefault();
+  console.log(formData);
   e.currentTarget.reset();
   localStorage.removeItem(LOCAL_KEY);
+
+  email.value = '';
+  message.value = '';
+  formData = {
+    email: '',
+    message: ''
+  };
 }
 
 const savedData = JSON.parse(localStorage.getItem(LOCAL_KEY));
@@ -32,5 +50,5 @@ if (savedData) {
     ...savedData,
   };
 }
-email.value = formData.email;
-message.value = formData.message;
+email.value = formData.email || '';
+message.value = formData.message || '';
